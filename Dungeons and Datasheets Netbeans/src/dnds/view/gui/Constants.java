e7f7d3fd376b95535af33cff9700ecbc503bbe15
Haschenohren <enmoniz@uncg.edu>
2019-10-03 17:30:45 -0400

    prototyping gui modularization

--- Dungeons and Datasheets Netbeans/src/dnds/view/gui/Constants.java
+++ Dungeons and Datasheets Netbeans/src/dnds/view/gui/Constants.java
@@ -3,8 +3,10 @@ package dnds.view.gui;
 import javafx.stage.Stage;
 
 public class Constants {
+
     static Stage stage;
-    final static String START_WINDOW = "StartWindow";
+    final static String START_WINDOW = "MainWindow";
+    final static String DEFAULT_SERVER_PANE = "components/server_pane/disconnected";
     final static String SERVER_WINDOW = "ServerWindow";
     final static String SETTINGS_WINDOW = "SettingsWindow";
     static String PREVIOUS_WINDOW;
