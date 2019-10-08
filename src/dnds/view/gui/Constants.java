943bf969a617b7b24aa89b546e005868671378b5
Haschenohren <enmoniz@uncg.edu>
2019-10-08 15:58:06 -0400

    Implemented ServerPane component, and controller for the disconnected view of the server pane

--- src/dnds/view/gui/Constants.java
+++ src/dnds/view/gui/Constants.java
@@ -4,10 +4,5 @@ import javafx.stage.Stage;
 
 public class Constants {
 
-    static Stage stage;
     final static String START_WINDOW = "MainWindow";
-    final static String DEFAULT_SERVER_PANE = "components/server_pane/disconnected";
-    final static String SERVER_WINDOW = "ServerWindow";
-    final static String SETTINGS_WINDOW = "SettingsWindow";
-    static String PREVIOUS_WINDOW;
 }
--- src/dnds/view/gui/DNDSApplication.java
+++ src/dnds/view/gui/DNDSApplication.java
@@ -17,22 +17,45 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * The {@code Application} class that controls the view.
+ *
+ * @author Eva Moniz
+ */
 public class DNDSApplication extends Application {
 
-    Parent root;
-    Stage stage;
-    String title = "Dungeons & DataSheets";
+    //The minimum bounds for the window
+    private static final int MIN_WINDOW_HEIGHT = 400;
+    private static final int MIN_WINDOW_WIDTH = 500;
 
+    /**
+     * The root node of the entire view
+     */
+    private Parent root;
+    /**
+     * The main application window
+     */
+    private Stage stage;
+    /**
+     * The title of the application
+     */
+    private String title = "Dungeons & DataSheets";
+
+    /**
+     * Starts the application, and the logic thread.
+     *
+     * @param primaryStage the application window
+     */
     @Override
     public void start(Stage primaryStage) {
 
         try {
-            root = FXMLLoader.load(Resources.getFxmlUrl(Constants.START_WINDOW));
+            this.root = FXMLLoader.load(Resources.getFxmlUrl(Constants.START_WINDOW));
 
-            stage = primaryStage;
-            stage.setTitle(title);
-            Constants.PREVIOUS_WINDOW = Constants.START_WINDOW;
-            Constants.stage = stage;
+            this.stage = primaryStage;
+            this.stage.setTitle(title);
+            this.stage.setMinHeight(DNDSApplication.MIN_WINDOW_HEIGHT);
+            this.stage.setMinWidth(DNDSApplication.MIN_WINDOW_WIDTH);
 
             Scene scene = new Scene(root);
             stage.setScene(scene);
