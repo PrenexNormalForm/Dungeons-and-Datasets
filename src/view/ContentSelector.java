2058e96810d5b01528591af910fb6643cc2fdeac
Haschenohren <enmoniz@uncg.edu>
2019-10-28 02:00:24 -0400

    GUI redesign

---
+++ src/view/ContentSelector.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+/*
+Last updated 2019-10-22
+
+
+
+Contributors:
+Eva Moniz
+ */
+
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author Eva Moniz
+ */
+public class ContentSelector extends VBox {
+
+}
