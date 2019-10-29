b59df7f223379cf2473a452bfd77eeed750a107f
Haschenohren <enmoniz@uncg.edu>
2019-10-29 16:55:18 -0400

    skeletal view controller, connected dummy welcome page

--- src/view/Constants.java
+++ src/view/Constants.java
@@ -4,5 +4,6 @@ import javafx.stage.Stage;
 
 public class Constants {
 
-    final static String START_WINDOW = "MainWindow";
+    final static String START_WINDOW = "ApplicationWindow";
+    final static String WELCOME_PAGE = "welcome.html";
 }
